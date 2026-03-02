ed9d22b26dbb0c7df6f53e6ccd9118adc1b40fe8
Salem Bamakhram <salemgaleb.3@gmail.com>
2026-03-03 02:50:59 +0300

    Delete project3 directory

--- project3/user-service/pages/api/users.js
+++
@@ -1,9 +0,0 @@
-
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    res.status(201).json({ message: 'User registered' });
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
-  }
-}
-                
